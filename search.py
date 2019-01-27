1e5c579ee3d1e1c60df0f254e8237da484c0d13f
Rodrigo Vicente <rodrigo@relax.mx>
2019-01-27 15:11:00 -0600

    add python script

---
+++ search.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+
+def searchFiles(path, file):
+	for root, dirs, files in os.walk(path, topdown=False):
+		for name in files:
+			if file == name:
+				print(os.path.join(root,name))
+
+
+
+def searchDirs(path, subdirectory):
+	for root, dirs, files in os.walk(path, topdown=False):
+		for name in dirs:
+			if subdirectory == name:
+				print(os.path.join(root,name))
+
+if len(sys.argv) > 2:
+	if sys.argv[1] == "-f":
+		searchFiles(sys.argv[2], sys.argv[3])
+	if sys.argv[1] == "-d":
+		searchDirs(sys.argv[2], sys.argv[3])
